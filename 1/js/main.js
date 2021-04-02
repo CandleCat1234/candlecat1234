a9c76cf8404722ffee7ccfca7f23f9d6d5b0dd5d
CandleCat1234 <81718356+CandleCat1234@users.noreply.github.com>
2021-04-01 21:23:48 -0400

    Create main.js

---
+++ 1/js/main.js
@@ -0,0 +1,23 @@
+const PLANCK_LENGTH = new Decimal(1.616229e-35);
+var dt1 = Date.now(), dt2 = Date.now();
+var keyMap = [];
+var saveTimer = 0;
+var stubbornComponents = ["layers", "body", "gamma", "tabNav"];
+var initialGame;
+
+var numberFormatters = [
+    new ADNotations.StandardNotation(),
+    new ADNotations.EngineeringNotation(),
+    new ADNotations.ScientificNotation(),
+    new ADNotations.LogarithmNotation(),
+    new ADNotations.InfinityNotation(),
+    new ADNotations.CancerNotation()
+]; // notations
+
+var app = new Vue({
+    el: "#app",
+    data: game,
+    computed: computed,
+    methods: functions,
+    created: onCreate
+});
