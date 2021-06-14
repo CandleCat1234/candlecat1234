4b1a3fb5b84ac8aa3483a3ffd98da87dddf7ebc6
CandleCat1234 <81718356+CandleCat1234@users.noreply.github.com>
2021-06-14 17:12:06 -0400

    Create player.js

---
+++ JToH: The Incremental/js/player.js
@@ -0,0 +1,16 @@
+
+var player = {
+  money: 10,
+  marketing: [],
+  artists: [],
+  lootboxTeam: []
+}
+
+var firstMarketing = {
+  cost: 10,
+  mult: 1,
+  amount: 0,
+  bought: 0
+}
+
+player.marketing.push(firstMarketing)
